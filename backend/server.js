eb5cfe53432f83cbf123636cb5375e57d611fa41
nazlitoyuq <gidrow2112@gmail.com>
2025-04-17 08:42:34 +0400

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-let users = [];
+const users = [];
 
 app.post('/register', (req, res) => {
-  const { nick, password } = req.body;
-  if (users.find(u => u.nick === nick)) {
+  const { nickname, password } = req.body;
+  if (users.find(u => u.nickname === nickname)) {
     return res.status(400).json({ message: 'Пользователь уже существует' });
   }
-  users.push({ nick, password });
-  res.json({ message: 'Пользователь зарегистрирован' });
+  users.push({ nickname, password });
+  res.status(201).json({ message: 'Регистрация успешна' });
 });
 
 app.post('/login', (req, res) => {
-  const { nick, password } = req.body;
-  const user = users.find(u => u.nick === nick && u.password === password);
+  const { nickname, password } = req.body;
+  const user = users.find(u => u.nickname === nickname && u.password === password);
   if (!user) {
     return res.status(401).json({ message: 'Неверные данные' });
   }
-  res.json({ message: 'Успешный вход' });
-});
-
-app.get('/search/:nick', (req, res) => {
-  const user = users.find(u => u.nick === req.params.nick);
-  if (!user) return res.json({});
-  res.json({ nick: user.nick });
+  res.json({ message: 'Вход успешен' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
